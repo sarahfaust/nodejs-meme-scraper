1bd594c021bd75cf99d0b60d8ec7ed392c501a5e
sarah faust <sarah.faustmann@gmail.com>
2021-11-23 22:43:12 +0100

    Integrate feedback from review

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const got = require('got');
 const cheerio = require('cheerio');
-const filesystem = require('node:fs');
+const fs = require('node:fs');
 
 const directory = './memes';
-const URL = 'https://memegen-link-examples-upleveled.netlify.app/';
 
 const topText = process.argv[2];
 const bottomText = process.argv[3];
@@ -14,8 +13,8 @@ const downloadImage = async (index, imageLink) => {
     const fileName = `${directory}/img-${index}.jpg`;
 
     // use got to create stream (instead of saving respose)
-    const downloadStream = got.stream(imageLink);
-    const fileWriterStream = filesystem.createWriteStream(fileName);
+    const downloadStream = await got.stream(imageLink);
+    const fileWriterStream = fs.createWriteStream(fileName);
     downloadStream.pipe(fileWriterStream);
   } catch (error) {
     console.log(error.response.body);
@@ -30,18 +29,17 @@ const getCustomImage = async (top, bottom, image) => {
     console.log(imageLink);
 
     // use got to create stream (instead of saving response)
-    const downloadStream = got.stream(imageLink);
-    const fileWriterStream = filesystem.createWriteStream(fileName);
+    const downloadStream = await got.stream(imageLink);
+    const fileWriterStream = fs.createWriteStream(fileName);
     downloadStream.pipe(fileWriterStream);
   } catch (error) {
-    console.log(error.response.body);
+    if (error.response.body) {
+      console.log(error.response.body);
+    } else {
+      console.error();
+      process.exit(1);
+    }
   }
-};
-
-const writeToConsole = (message) => {
-  process.stdout.clearLine();
-  process.stdout.cursorTo(0);
-  process.stdout.write(message);
 };
 
 const getRandomImages = async (url) => {
@@ -53,12 +51,12 @@ const getRandomImages = async (url) => {
     // use cheerio to extract <img> tags
     // this returns a mass object, not an array
     const $ = cheerio.load(html);
-    const linkObjects = $('img');
+    const linkStringList = $('img');
 
     // collect the "src" of each link and add them to a new array
-    const links = [];
-    linkObjects.each((index, element) => {
-      links.push($(element).attr('src'));
+    const imageLinkList = [];
+    linkStringList.each((index, element) => {
+      imageLinkList.push($(element).attr('src'));
     });
 
     // define variables for progress bar
@@ -77,7 +75,7 @@ const getRandomImages = async (url) => {
 
     // loop through link array
     for (let i = 0; i < 10; i++) {
-      downloadImage(i, links[i]);
+      downloadImage(i, imageLinkList[i]);
 
       // track progress
       const progress = '#####';
@@ -87,15 +85,28 @@ const getRandomImages = async (url) => {
       const consoleMessage = `Downloading images: |${progressBar}${waitBar}| ${percent}%`;
 
       // clear sdtout and print current message
-      setTimeout(() => writeToConsole(consoleMessage), 500 * i);
+      setTimeout(() => {
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        process.stdout.write(consoleMessage);
+      }, 200 * i);
     }
+
+    setTimeout(() => {
+      console.log('\nDownload finished!');
+    }, 2000);
   } catch (error) {
-    console.log(error.response.body);
+    if (error.response.body) {
+      console.log(error.response.body);
+    } else {
+      console.error();
+      process.exit(1);
+    }
   }
 };
 
 if (topText && bottomText && memeImage) {
   getCustomImage(topText, bottomText, memeImage);
 } else {
-  getRandomImages(URL);
+  getRandomImages('https://memegen-link-examples-upleveled.netlify.app/');
 }
